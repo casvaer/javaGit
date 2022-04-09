b46d3f316696a6efc118202168230c702de14cc7
chenshiqi <csq1371414102@163.com>
2022-04-09 23:20:38 +0800

    添加JUnit依赖包 测试JUnit5个测试注解

--- src/main/java/com/example/javagit/dao/UserDao.java
+++ src/main/java/com/example/javagit/dao/UserDao.java
@@ -1,8 +1,40 @@
 package com.example.javagit.dao;
 
-import com.example.javagit.controller.User;
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
+import org.junit.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class UserDao {
+    public static void main(String[] args) {
+        //关掉拼写检查
+        List<Object> objectList = Arrays.asList(new String[]{"chenshiqi"});
+        System.out.print(objectList.toString());
+    }
+
+    @Test
+    public void test(){
+        System.out.println("hello world!");
+    }
+
+    @BeforeClass
+    public static void before(){
+        System.out.println("开始之前");
+    }
+
+    @AfterClass
+    public static void after(){
+        System.out.println("完成之后");
+    }
+
+    @Before
+    public void willbefore(){
+        System.out.println("要开始了……");
+    }
+
+    @After
+    public void willafter(){
+        System.out.println("要结束了……");
+    }
 }
