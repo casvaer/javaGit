ac84dd92052b624501eb0aa38bf143b558012d85
chenshiqi <csq1371414102@163.com>
2022-04-10 21:25:12 +0800

    在RoleMapper文件中添加了一行代码

--- src/main/java/com/example/javagit/dao/RoleMapper.java
+++ src/main/java/com/example/javagit/dao/RoleMapper.java
@@ -1,7 +1,10 @@
 package com.example.javagit.dao;
 
+import com.example.javagit.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface RoleMapper {
+
+    User getStirng();
 }
